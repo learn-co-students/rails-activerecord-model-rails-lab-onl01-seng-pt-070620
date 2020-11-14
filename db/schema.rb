92c0a891b4b6e6b0feefc61c1828e3ed51a27c54
keesha <kharg14@gmail.com>
2020-11-13 21:36:27 -0500

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,11 @@
 
 ActiveRecord::Schema.define(version: 1) do
 
-  create_table "users", force: :cascade do |t|
-    t.string "name"
-    t.string "lastname"
+  create_table "students", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
